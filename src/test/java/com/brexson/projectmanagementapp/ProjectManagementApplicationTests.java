b81d7a6deaaaaf940435d582c194aa76ff3d9c11
steevecode <stevecode@yahoo.com>
2023-03-14 13:29:39 +0100

    updated name of application main class

--- src/test/java/com/brexson/projectmanagementapp/ProjectManagementApplicationTests.java
+++ src/test/java/com/brexson/projectmanagementapp/ProjectManagementApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HrappApplicationTests {
+class ProjectManagementApplicationTests {
 
     @Test
     void contextLoads() {
